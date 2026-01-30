e993513bc7edc7936a4b4b21d3b6bdc04962b813
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2026-01-30 10:41:14 -0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Driver, Car, Manufacturer
 from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
+from .models import Driver, Car, Manufacturer
 
 @login_required
 def index(request):
